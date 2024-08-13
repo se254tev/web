29d30addb85528248cf781bb92bb868fb26367e6
se254tev <131610586+se254tev@users.noreply.github.com>
2024-08-13 06:38:40 +0300

    Delete assets/js directory

--- assets/js/announcementsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const announcementController = require('../controllers/announcementController');
-
-router.post('/add', announcementController.addAnnouncement);
-router.get('/summaries', announcementController.getAnnouncementSummaries);
-router.get('/details', announcementController.getAnnouncementDetails);
-
-module.exports = router;
