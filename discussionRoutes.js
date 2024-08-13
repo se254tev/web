df7a373e12858e84a5b77431384d6c3ea6750ef6
se254tev <131610586+se254tev@users.noreply.github.com>
2024-08-13 07:39:52 +0300

    Delete discussionRoutes.js

--- discussionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const discussionController = require('../controllers/discussionController');
-
-router.post('/add', discussionController.addDiscussion);
-router.get('/summaries', discussionController.getDiscussionSummaries);
-router.get('/details', discussionController.getDiscussionDetails);
-
-module.exports = router;
